63b5e3cbbd18f10c48bbe36e5585a79c63277c91
James2.0 <jamesgnagne3@gmail.com>
2023-01-07 13:15:06 +0100

    ez

--- ex03/src/SearchForm.jsx
+++ ex03/src/SearchForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const SearchForm = (props) => {
-    console.log(props.value);
     return (
         <form onSubmit={props.handleSubmit}>
             <input onChange={props.handleChange} type="text" value={props.value} />
